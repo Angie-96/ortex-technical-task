38a1dc07fc6eee1ff1bdec39d241a927522d6c8b
Angie <hi@angie.digital>
2021-11-19 16:10:57 -0300

    Add svg loader for market prices

---
+++ components/Common/Loaders/MarketPricesLoader/index.tsx
@@ -0,0 +1,29 @@
+import ContentLoader from 'react-content-loader';
+
+const MarketPricesLoader = () => (
+  <ContentLoader
+    backgroundColor="#ffffff21"
+    foregroundColor="#ffffff45"
+    viewBox="0 0 102 120"
+  >
+    <circle cx="40%" cy="20%" r="18" />
+    <circle cx="57%" cy="35%" r="18" />
+    <rect x="10" y="60%" rx="4" ry="4" width="80%" height="11" />
+    <rect x="10" y="80%" rx="3" ry="3" width="60%" height="8" />
+  </ContentLoader>
+);
+
+const MarketPricesLoaderDesktop = () => (
+  <ContentLoader
+    backgroundColor="#ffffff21"
+    foregroundColor="#ffffff45"
+    viewBox="0 0 250 80"
+  >
+    <circle cx="13%" cy="41%" r="18" />
+    <circle cx="20%" cy="61%" r="18" />
+    <rect x="80" y="26" rx="4" ry="4" width="140" height="11" />
+    <rect x="80" y="46" rx="3" ry="3" width="130" height="8" />
+  </ContentLoader>
+);
+
+export { MarketPricesLoader, MarketPricesLoaderDesktop };
