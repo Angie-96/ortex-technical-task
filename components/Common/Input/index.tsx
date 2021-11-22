ca13483cf13f0cb0e1010ac058973c926302e37c
Angie <hi@angie.digital>
2021-11-22 13:03:38 -0300

    Fix form validation

--- components/Common/Input/index.tsx
+++ components/Common/Input/index.tsx
@@ -1,42 +1,27 @@
 import React from 'react';
 import Image from 'next/image';
-import { useForm } from 'react-hook-form';
 
 import styles from './Input.module.scss';
 
 interface InputProps {
-  inputName: string;
   placeholder: string;
   type: string;
   icon: string;
-  inputValidation: object;
+  register: object;
+  error: string;
 }
 
-const Input = ({
-  inputName,
-  placeholder,
-  type,
-  icon,
-  inputValidation,
-}: InputProps) => {
-  const {
-    register,
-    formState: { errors },
-  } = useForm();
-
+const Input = ({ placeholder, type, icon, register, error }: InputProps) => {
   return (
-    <div className={styles.inputContainer}>
-      <div className={styles.inputIcon}>
-        <Image src={icon} alt="Input Logo" width={25} height={25} />
+    <>
+      <div className={styles.inputContainer}>
+        <div className={styles.inputIcon}>
+          <Image src={icon} alt="Input Logo" width={25} height={25} />
+        </div>
+        <input type={type} placeholder={placeholder} {...register} />
       </div>
-      <input
-        type={type}
-        placeholder={placeholder}
-        {...(register(inputName), () => inputValidation)}
-      />
-
-      {errors && <span className="isRequired">{errors.message}</span>}
-    </div>
+      {error}
+    </>
   );
 };
 
--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,60 +1,111 @@
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Button } from '../Common/Button';
 import { Input } from '../Common/Input';
 import { Modal } from '../Common/Modal';
+
 import styles from './Login.module.scss';
 
+interface formDataProps {
+  email: string;
+  password: string;
+}
+
 const Login = () => {
-  const { handleSubmit, reset } = useForm();
+  const {
+    handleSubmit,
+    reset,
+    register,
+    formState: { errors },
+  } = useForm();
+
   const [showModal, setShowModal] = React.useState(false);
 
-  const onSubmit = (formData: object) => {
-    console.log(formData);
+  const onSubmit = (formData: formDataProps) => {
+    const userData = formData;
+    console.log(userData);
+    alert('You logged in successfully');
     reset();
   };
 
-  const inputItems = [
-    {
-      inputName: 'email',
-      placeholder: 'Email',
-      type: 'text',
-      icon: '/icons/at.svg',
-      inputValidation: {
-        required: true,
+  const sectionVariants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5,
       },
     },
-    {
-      inputName: 'password',
-      placeholder: 'Password',
-      type: 'password',
-      icon: '/icons/password.svg',
-      inputValidation: {
-        required: true,
-        minLength: {
-          value: 4,
-          message: 'Password min length is 4.',
-        },
-        maxLength: {
-          value: 8,
-          message: 'Password max length is 8.',
-        },
-      },
-    },
-  ];
+  };
+
+  const childrenVariants = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+  };
 
   return (
-    <section className={styles.formSection}>
-      <div className={styles.formHeader}>
+    <motion.section
+      className={styles.formSection}
+      variants={sectionVariants}
+      initial="hidden"
+      animate="show"
+    >
+      <motion.div variants={childrenVariants} className={styles.formHeader}>
         <Image src="/ortex.svg" alt="Ortex Logo" width={130} height={67} />
         <p>Welcome back!</p>
-      </div>
-      <form onSubmit={handleSubmit(onSubmit)} className={styles.loginForm}>
-        {inputItems.map((el) => (
-          <Input key={el.inputName} {...el} />
-        ))}
+      </motion.div>
+      <motion.form
+        variants={childrenVariants}
+        onSubmit={handleSubmit(onSubmit)}
+        className={styles.loginForm}
+      >
+        <Input
+          placeholder="Email"
+          type="text"
+          icon="/icons/at.svg"
+          register={{
+            ...register('email', {
+              required: 'Email is required.',
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: 'Invalid email address.',
+              },
+            }),
+          }}
+          error={
+            errors.email && (
+              <span className={styles.isRequired}>{errors.email.message}</span>
+            )
+          }
+        />
+        <Input
+          placeholder="Password"
+          type="password"
+          icon="/icons/password.svg"
+          register={{
+            ...register('password', {
+              required: 'Password is required.',
+              minLength: {
+                value: 4,
+                message: 'Password min length is 4.',
+              },
+              maxLength: {
+                value: 8,
+                message: 'Password max length is 8.',
+              },
+            }),
+          }}
+          error={
+            errors.password && (
+              <span className={styles.isRequired}>
+                {errors.password.message}
+              </span>
+            )
+          }
+        />
         <div className={styles.recoverPassword}>
           <Button
             text="Forgot your password?"
@@ -62,7 +113,7 @@ const Login = () => {
           />
         </div>
         <Button text="Submit" action={handleSubmit(onSubmit)} />
-      </form>
+      </motion.form>
       <AnimatePresence
         initial={false}
         exitBeforeEnter={true}
@@ -75,7 +126,7 @@ const Login = () => {
           />
         )}
       </AnimatePresence>
-    </section>
+    </motion.section>
   );
 };
 
