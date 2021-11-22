8ae3b6fe2247fe57ca4d80a545261d75297265f0
Angie <hi@angie.digital>
2021-11-22 13:21:13 -0300

    Add background video as local file

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -5,10 +5,7 @@ const Layout = ({ children }: any) => {
     <>
       <div className={styles.videoBgContainer}>
         <video loop autoPlay>
-          <source
-            src="https://public.ortex.com/wp-content/uploads/2020/09/iStock-1002667230-1.mp4"
-            type="video/mp4"
-          />
+          <source src="/video/ortex-bg-video.mp4" type="video/mp4" />
         </video>
       </div>
       <div className={styles.childrenContainer}>{children}</div>
