f5470384900a51a524125eda06c0c1a710011053
Angie <hi@angie.digital>
2021-12-13 22:20:15 -0300

    Add comment

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -9,6 +9,8 @@ export default function handler(
   res: NextApiResponse<Data>,
 ) {
   if (req.method === 'POST') {
+    //Here should be the proper auth,
+    //for now it just sends the request body(email and password) as the response.
     res.status(200).json(req.body);
   } else {
     res.status(405).send({ message: 'Only POST requests allowed' });
